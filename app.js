3476aa97159223ff84aae699a85716c6f6054a82
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-01-21 15:57:07 +0200

    added create new campground

--- app.js
+++ app.js
@@ -29,10 +29,21 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show', { campground });
+});
+
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, () => {
