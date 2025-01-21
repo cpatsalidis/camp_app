bcbf913b970e4425966ab7f2dc7f0504846863e3
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-01-21 13:31:57 +0200

    added get /campgrounds to display all campground names in database

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/camp_app')
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
