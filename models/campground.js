a0bf628c5dc35bf29998d7bb1d5e7615b8f97b11
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-05 13:00:00 +0200

    added author to cmapground model

--- models/campground.js
+++ models/campground.js
@@ -21,6 +21,10 @@ const CampgroundSchema = new Schema({
         type: String,
         required: true
     },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
