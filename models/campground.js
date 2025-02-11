6ca914d4a2e8db11cdc622103b9ff9f53eb1ba70
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-11 12:00:00 +0200

    added thumbnail

--- models/campground.js
+++ models/campground.js
@@ -7,6 +7,11 @@ const ImageSchema = new Schema({
     filename: String
 });
 
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
     title: {
         type: String,
