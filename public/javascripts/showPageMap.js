65972d872224b3c341094dc9d5bb4f9a1233fa72
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-12 12:00:00 +0200

    added clusterMap

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,9 +2,9 @@ maptilersdk.config.apiKey = maptilerApiKey;
 
 const map = new maptilersdk.Map({
     container: 'map',
-    style: maptilersdk.MapStyle.BRIGHT,
+    style: maptilersdk.MapStyle.STREETS,
     center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 10 // starting zoom
+    zoom: 12 // starting zoom
 });
 
 new maptilersdk.Marker()
