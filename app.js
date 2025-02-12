37dd2dbbb735eac5d8c59f572e9f0b281df1a108
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-12 12:00:00 +0200

    added helmet

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const ExpressError = require('./utils/ExpressError');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
@@ -42,10 +43,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
 // app.use(morgan('tiny'));
 
 
 const sessionConfig = {
+    name: 'user_sess',
     secret: 'mysecret',
     resave: false,
     saveUninitialized: true,
