bdd76551123dba69cd28fc2fc97a9f350de79154
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-12 12:00:00 +0200

    modified seed to include geometry

--- seeds/index.js
+++ seeds/index.js
@@ -19,14 +19,18 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const city = cities[random1000];
         const price = Math.floor(Math.random() * 20) + 10;
-        const randomImg = Math.random();
         const camp = new Campground({
             author: '67ab85490bc1c01ce129d4dd',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            location: `${city.city}, ${city.state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price: price,
+            geometry: {
+                type: 'Point',
+                coordinates: [city.longitude, city.latitude]
+            },
             images: [
                 {
                     url: 'https://res.cloudinary.com/dtyc5nqsr/image/upload/v1739381505/Campapp/omqtiw8awziapmr9evxq.jpg',
