02460fede1667c71bde464626e90f0527fa59b3d
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-12 12:00:00 +0200

    added popUpMArkup virtual nested in properties

--- models/campground.js
+++ models/campground.js
@@ -52,6 +52,12 @@ const CampgroundSchema = new Schema({
     ]
 }, opts);
 
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
+});
+
 CampgroundSchema.post('findOneAndDelete', async doc => {
     if (doc) {
         await Review.deleteMany({
