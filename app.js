0093152d7dc9d598808f28abeafdbc2aa86ec039
cpatsalidis <christos.patsalidis20@imperial.ac.uk>
2025-02-13 12:00:00 +0200

    port-log

--- app.js
+++ app.js
@@ -152,5 +152,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-    console.log('Server is running on port 3000');
+    console.log(`Server is running on port ${port}`);
 });
